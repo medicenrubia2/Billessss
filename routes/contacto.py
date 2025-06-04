2aba9d7a06959598a63e418d0667191fc3a0de16
Mateo Mateo <istemsd@gmail.com>
2025-06-04 10:18:23 -0400

    Fix import path after moving database folder

--- routes/contacto.py
+++ routes/contacto.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
-from backend.database.connection import SessionLocal
+from database.connection import SessionLocal
 from ..database.models import Contacto
 
 router = APIRouter()
